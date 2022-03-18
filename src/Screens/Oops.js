13365ab1d219e270f8579efb71d67acbabf341a8
ThatoMahloko <thato732mahloko@gmail.com>
2022-03-18 14:19:31 +0200

    Opps Page

--- src/Screens/Oops.js
+++ src/Screens/Oops.js
@@ -1,12 +1,28 @@
 import { View, Text } from 'react-native'
 import { styles } from '../../assets/styles/styles'
+import Outdoor from '../../assets/icons/Outdoor'
+import { Avatar, FAB } from 'react-native-elements'
 import React from 'react'
 
-export default function Oops({route}) {
+export default function Oops() {
   return (
     <View style={styles.container}>
-      <Text>Oops</Text>
-      <Text>{route.params.id}</Text>
+      <View style={styles.avatars}>
+        <FAB
+          visible={true}
+          icon={{ name: 'left', type: 'ant-design', color: '#FF9F45' }}
+          color="white"
+          style={{ right: '35%' }}
+          onPress={() => navigation.goBack()}
+        />
+        <Text style={{ right: '50%', fontFamily: 'Roboto_300Light' }}>Oops</Text>
+
+      </View>
+      <Text style={{ fontFamily: 'Roboto_700Bold', color: '#FCB330', fontSize: 60 }}>OOPS!</Text>
+      <Text style={{ fontFamily: 'Roboto_700Bold', color: '#FCB330', fontSize: 60 }}>Error</Text>
+      <View style={styles.onBoardingContainer}>
+        <Outdoor />
+      </View>
     </View>
   )
 }
